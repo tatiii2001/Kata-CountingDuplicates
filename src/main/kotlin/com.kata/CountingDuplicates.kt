028c432015c261cd0000c1d78115b8c574b7978a
tatiii2001 <tatiiigomez2001@gmail.com>
2024-04-01 15:01:33 +0100

    Create the CountingDuplicates class and implement the function for count the letters duplicates in the same word, and implement the test for different scenarios

---
+++ src/main/kotlin/com.kata/CountingDuplicates.kt
@@ -0,0 +1,19 @@
+package com.kata
+
+class CountingDuplicates(private val word: String) {
+    fun countDuplicate(): Int {
+        val chars = this.word.uppercase().toCharArray()
+        val eachTypeOfChar = chars.toSet()
+        var countDuplicates = 0
+
+        eachTypeOfChar.forEach { char ->
+            val countLetter = chars.count { it == char }
+            if (countLetter > 1){
+                countDuplicates ++
+            }
+        }
+
+        return countDuplicates
+    }
+
+}
---
+++ src/test/kotlin/com/kata/CountingDuplicatesShould.kt
@@ -0,0 +1,54 @@
+package com.kata
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class CountingDuplicatesShould : DescribeSpec({
+    /*
+    *   Only string and numeric input values
+    *   Return the number of repeated elements
+     */
+
+    describe("return the number of repeated elements"){
+        it("repeated in the word abbadc12"){
+            val word = CountingDuplicates("abbadc12")
+
+            val result = word.countDuplicate()
+
+            result.shouldBe(2)
+        }
+
+        it("repeated in the word indivisibility"){
+            val word = CountingDuplicates("indivisibility")
+
+            val result = word.countDuplicate()
+
+            result.shouldBe(1)
+        }
+
+        it("repeated in the word Indivisibilities"){
+            val word = CountingDuplicates("Indivisibilities")
+
+            val result = word.countDuplicate()
+
+            result.shouldBe(2)
+        }
+
+        it("repeated in the word aA11"){
+            val word = CountingDuplicates("aA11")
+
+            val result = word.countDuplicate()
+
+            result.shouldBe(2)
+        }
+
+        it("repeated in the word ABb4588lkkoopyy"){
+            val word = CountingDuplicates("ABb4588lkkoopyy")
+
+            val result = word.countDuplicate()
+
+            result.shouldBe(5)
+        }
+    }
+
+})
